6841dddafb67e66a32e8e36758cd6bc7a2234b4a
Nicole Hill <nicole@poissonconsulting.ca>
2023-05-09 16:56:29 -0700

    Remove unnecessary script

--- copy-database.R
+++
@@ -1,6 +0,0 @@
-source("header.R")
-
-sbf_copy_db(file.path("~/Poisson/Data/fish-passage/2022/dbs",
-                      "fish-passage-21.sqlite"),
-            db_name = "fish-passage-21", 
-            exists = FALSE)
