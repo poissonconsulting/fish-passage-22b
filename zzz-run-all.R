1817aa8f452c95434c500acbe0c6bc507c8d4085
Nicole Hill <nicole@poissonconsulting.ca>
2023-09-18 13:17:12 -0700

    Remove ssn scripts

--- zzz-run-all.R
+++ zzz-run-all.R
@@ -14,18 +14,10 @@ source("read-data.R")
 source("clean-data.R")
 source("tidy-data.R")
 
-# .ssn object for SSN and SSNbayes 
-source("read-elev.R")
-source("get-ssn.R") # takes > 1 day 
-# source("ssnbayes.R")
-
 source("plot-water-temp.R")
 
-source("import-database.R")
 source("create-database.R")
 source("populate-database.R")
-
-source("session-info.R")
 
 if(FALSE) {
   source("knit-report.R")
