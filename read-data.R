c94b438878179f8a85077b09225bd72ce7d9e0e0
Nicole Hill <nicole@poissonconsulting.ca>
2023-09-18 13:17:26 -0700

    Download water temp programatically

--- read-data.R
+++ read-data.R
@@ -3,33 +3,48 @@ source("header.R")
 sbf_set_sub("read")
 
 # Water temp ----
-# Data downloaded from https://zenodo.org/record/6426024#.ZEAqr-zMI0Q on 04-19-2023
-dir <- file.path("~/Poisson/Data/fish-passage/2022/Data/Nechako water temp")
+# Data downloaded programatically f
+# Backup data downloaded from https://zenodo.org/record/6426024#.ZEAqr-zMI0Q on 04-19-2023
+dir <- "~/Poisson/Data/fish-passage/2022/Data/Nechako water temp"
 
-water_temp <-
-  list.files(
-    path = file.path(dir, "Data"),
-    full.names = TRUE,
-    pattern = "*.csv"
-  ) %>% 
-  map(.x = ., \(x) read_csv(x, id = "file")) %>%
-  list_rbind()
+# Water temp ----
+temp <- tempfile()
+download.file("https://zenodo.org/record/6426024/files/Data.zip?download=1", temp)
+file_names <- unzip(temp, list = TRUE)$Name
+unzip(temp)
 
-water_temp_meta <- 
-  read_xlsx(file.path(dir, "water_temp_meta_data.xlsx"), col_names = "meta")
+water_temp <- list()
+for (i in 1:length(file_names)) {
+  water_temp[[i]] <- read_csv(file_names[i], id = "file")
+}
+
+water_temp <- list_rbind(water_temp)
+
+unlink(temp)
+rm(temp)
+
+# metadata
+download.file(
+  "https://zenodo.org/record/6426024/files/NHG%20Data%20ReadMe.txt?download=1",
+  "output/data/read/water_temp_meta_data.txt"
+)
+
+water_temp_meta_data <- read_tsv("output/data/read/water_temp_meta_data.txt", skip = 3)
   
 # Air temp ----
 # This is air temp for July 2019 downloaded on 04/25/23 for the area 
 # longitude: -127.53 to -122.92
 # latitude: 23.32 to 54.89 
 # from: https://cds.climate.copernicus.eu/cdsapp#!/dataset/reanalysis-era5-land?tab=form
+### Not going to be able to programatically download this data. 
 dir <- file.path("~/Poisson/Data/fish-passage/2022/Data/Air temp/data.nc")
+# dir <- file.path("~/Poisson/Clients - Transfer/New Graph/xxxxxxxx")
 
-ncin <- nc_open(dir)
-origin <- ncin$dim$time$units # UTC
-nc_close(ncin)
-
-rm(ncin)
+# ncin <- nc_open(dir)
+# origin <- ncin$dim$time$units # UTC
+# nc_close(ncin)
+# 
+# rm(ncin)
 
 air_temp <- tidync(dir) %>%  # Can filter this before making it a tibble using 
   # `hyper_filter()` https://rdrr.io/cran/tidync/f/vignettes/netcdf-with-tidync.Rmd
