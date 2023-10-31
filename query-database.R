e446fb8a4fe3647427b1bc7d4e1c0bf735844459
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-31 14:38:38 -0700

    Drop units

--- query-database.R
+++ query-database.R
@@ -2,5 +2,9 @@ source("header.R")
 
 sbf_load_datas_from_db()
 
+water_temp <- drop_units(water_temp)
+discharge <- drop_units(discharge)
+discharge_site <- drop_units(discharge_site)
+
 sbf_set_sub("query")
 sbf_save_datas()
