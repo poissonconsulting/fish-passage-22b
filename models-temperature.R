5633d58b715083a86d5f21d9aec1ed910593def7
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-31 08:58:46 -0700

    Use only sites with discharge in matrices

--- models-temperature.R
+++ models-temperature.R
@@ -24,16 +24,18 @@ model <- model(
     data$N_y_mis <- length(data$i_y_mis)
     data$y_obs <- data$mean_temp[!is.na(data$mean_temp)]
     data$I <- diag(1, nrow = data$nsite, ncol = data$nsite)
-    data$D <- D
-    data$W <- W
-    data$H <- H
-    data$flow_con_mat <- flow_con_mat
-    data$E <- E
+    data$D <- D[c("LTL", "TPL"), c("LTL", "TPL")]
+    data$W <- W[c("LTL", "TPL"), c("LTL", "TPL")]
+    data$H <- H[c("LTL", "TPL"), c("LTL", "TPL")]
+    data$flow_con_mat <- flow_con_mat[c("LTL", "TPL"), c("LTL", "TPL")]
+    data$E <- E[c("LTL", "TPL"), c("LTL", "TPL")]
+    # data$spec_heat_water <- 1000 # kg/m3
+    # data$density_water <- 41.8e3 # J/kg/˚C
     data
   },
   new_expr = "
     for (i in 1:nObs) {
-      prediction[i] <- exp(bIntercept + bDoy * doy[i] + bDoy2 * doy[i]^2)
+      prediction[i] <- bIntercept + bShortwave * shortwave[i] + bDischarge * mean_discharge[i];
       # Predict over range of H values (total hydrologic distance)
       eTU[i] <- sigma_tu^2 * exp(-3 * H[i] / alpha_tu)
       eTD[i] <- sigma_td^2 * exp(-3 * H[i] / alpha_td) # Flow-unconnected just sums distance to common confluence
@@ -41,10 +43,11 @@ model <- model(
     }",
   new_expr_vec = TRUE,
   select_data = list(
-    mean_temp = c(0, 30, NA),
+    mean_temp = c(-2, 30, NA),
     site = factor(),
     week = factor(),
-    `doy*` = c(1L, 366L),
+    `mean_discharge*` = c(0, 50),
+    `shortwave*` = c(50, 1000),
     H = c(0, 50 * 5000),
     E = c(0, 50 * 500)
   )
