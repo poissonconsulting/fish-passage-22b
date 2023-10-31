6434017cc0516432a16bd3d571b4e2f942d0b239
Nicole Hill <nicole@poissonconsulting.ca>
2023-10-31 08:59:15 -0700

    Add shortwave and discharge data

--- manipulate-temperature.R
+++ manipulate-temperature.R
@@ -11,90 +11,96 @@ E <- euclidean_distance / 1000
 dist_sites <- rownames(D)
 
 sbf_load_datas("query")
-
-# # Daily mean water temp ----
-# water_temp <-
-#   water_temp %>% 
-#   filter(flag == "P") %>% 
-#   select(site, date, time, temp) %>% 
-#   group_by(site, date) %>% 
-#   summarize(mean_temp = mean(temp), .groups = "drop")
-# 
-# data <- water_temp %>% 
-#   expand(site, date = full_seq(date, 1)) %>% 
-#   left_join(water_temp, join_by(site, date)) %>% 
-#   mutate(
-#     site_remains = if_else2(site %in% dist_sites, TRUE, FALSE)
-#   ) %>% 
-#   filter(site_remains) %>% 
-#   select(site, date, mean_temp)
-# 
-# ### Filter to dates with at least 3 sites with data
-# keep_dates <- 
-#   data %>% 
-#   filter(!is.na(mean_temp)) %>% 
-#   count(date) %>% 
-#   filter(n >= 3) %>% 
-#   pull(date)
-# 
-# data %<>% 
-#   filter(date %in% keep_dates) %>% 
-#   mutate(
-#     doy = dtt_doy(date),
-#     date = factor(date),
-#     site = factor(site)
-#   ) %>% 
-#   ### Must be arranged in this order for the model
-#   arrange(date, site)
-# 
-# chk_equal(levels(data$site), dist_sites)
+sbf_load_datas("radiation")
 
 # Weekly mean water temp ----
-water_temp <-
-  water_temp %>% 
-  filter(flag == "P") %>% 
-  select(site, date, time, temp) %>% 
+date_ref <-
+  tibble(date = seq(min(water_temp$date), max(water_temp$date), by = "day")) %>% 
   mutate(
-    day = as.integer(dtt_date(date)),
+    annual = dtt_year(date),
+    day = as.integer(date),
     day = day - min(day),
-    week = as.integer(day %/% 7 + 1)
+    week = as.integer(day %/% 7 + 1),
+    doy = dtt_doy(date)
   ) %>% 
   group_by(week) %>% 
-  mutate(doy = min(dtt_doy(date))) %>% 
-  group_by(site, week, doy) %>% 
-  summarize(mean_temp = mean(temp), .groups = "drop")
+  arrange(annual, doy, .by_group = TRUE) %>% 
+  summarize(
+    annual = first(annual), 
+    doy = first(doy), 
+    .groups = "drop"
+  )
 
-week_doy <- 
+# Check full seq of dates in discharge data
+chk_equal(
+  unique(discharge$date) %>% sort(), 
+  seq(min(discharge$date), max(discharge$date), 1)
+)
+
+water_temp <- 
   water_temp %>% 
-  select(week, doy) %>% 
-  distinct() %>% 
-  arrange(week)
+  ### TODO: Decide what flags to keep?
+  filter(flag != "F") %>% 
+  mutate(
+    annual = dtt_year(date),
+    day = as.integer(date),
+    day = day - min(day),
+    week = as.integer(day %/% 7 + 1),
+    doy = dtt_doy(date)
+  ) %>% 
+  # add in discharge data
+  left_join(discharge, join_by(site, date)) %>% 
+  group_by(site, week) %>%
+  summarize(
+    mean_temp = mean(temp), 
+    mean_level = mean(level),
+    mean_discharge = mean(discharge),
+    .groups = "drop"
+  ) %>% 
+  drop_na(mean_level)
+
+chk_equal(
+  unique(water_temp$week) %>% sort(), 
+  seq(min(water_temp$week), max(water_temp$week), 1)
+)
 
 data <- 
   water_temp %>% 
   expand(site, week = full_seq(week, 1)) %>% 
   left_join(water_temp, join_by(site, week)) %>% 
-  left_join(week_doy, join_by(week)) %>% 
+  left_join(date_ref, join_by(week), relationship = "many-to-one") %>% 
+  # Mean open sky shortwave radiation in weekly period
+  # Calculated by minute during water temp recording period
+  # Summed over each day
+  # And mean by week
+  left_join(radiation, join_by(site, week)) %>%
   mutate(
-    doy = if_else(is.na(doy.x), doy.y, doy.x),
     site_remains = if_else2(site %in% dist_sites, TRUE, FALSE)
   ) %>% 
   filter(site_remains) %>% 
-  select(site, week, doy, mean_temp)
+  select(site, week, doy, annual, mean_temp, mean_discharge, mean_level, shortwave) %>% 
+  group_by(week) %>% 
+  mutate(
+    mean_volume = mean_discharge,
+    drop_week = if_else2(any(is.na(mean_level)), TRUE, FALSE)
+  ) %>% 
+  filter(!drop_week) %>% 
+  ungroup()
+  
 
-### Filter to dates with at least 3 sites with data
-keep_weeks <- 
-  data %>% 
-  filter(!is.na(mean_temp)) %>% 
-  dplyr::count(week) %>% 
-  filter(n >= 3) %>% 
-  pull(week)
+# ### Filter to dates with at least 3 sites with data
+# keep_weeks <-
+#   data %>%
+#   filter(!is.na(mean_temp)) %>%
+#   dplyr::count(week) %>%
+#   filter(n >= 3) %>%
+#   pull(week)
+# 
+# # check no gaps in weeks
+# chk_equal(keep_weeks, min(keep_weeks):max(keep_weeks))
 
-# check no gaps
-chk_equal(keep_weeks, min(keep_weeks):max(keep_weeks))
-
-data %<>% 
-  filter(week %in% keep_weeks) %>% 
+data %<>%
+  # filter(week %in% keep_weeks) %>%
   mutate(
     week = factor(week),
     site = factor(site),
@@ -104,10 +110,17 @@ data %<>%
   ### Must be arranged in this order for the model
   arrange(week, site)
 
-chk_equal(levels(data$site), dist_sites)
+# chk_equal(levels(data$site), dist_sites)
 
 gp <- ggplot(data) +
   geom_line(aes(x = doy, y = mean_temp, group = site, color = site)) +
+  NULL
+
+sbf_open_window(6, 6)
+sbf_print(gp)
+
+gp <- ggplot(data) +
+  geom_line(aes(x = doy, y = shortwave, group = site, colour = site)) +
   NULL
 
 sbf_open_window(6, 6)
