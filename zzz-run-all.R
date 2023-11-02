09a529d63862d4e9285593a03fa7e68c67cf39f1
Nicole Hill <nicole@poissonconsulting.ca>
2023-11-02 15:23:17 -0700

    Update run-all

--- zzz-run-all.R
+++ zzz-run-all.R
@@ -14,25 +14,33 @@ source("read-water-temp.R")
 source("clean-water-temp.R")
 source("tidy-water-temp.R")
 
-source("read-air-temp.R")
-source("clean-air-temp.R")
-source("tidy-air-temp.R")
-
 source("read-discharge.R")
 source("clean-discharge.R")
 source("tidy-discharge.R")
+
+source("read-weather.R")
+source("clean-weather.R")
+source("tidy-weather.R")
 
 source("create-database.R")
 source("query-database.R")
 
 source("plot-water-temp.R")
-# source("plot-air-temp.R")
 source("plot-discharge.R")
+source("plot-weather.R")
 
-source("download-distances.R")
-source("calculate-shortwave-radiation.R")
+source("SSN-distances.R")
 
-source("manipulate-temperature.R")
+source("prepare-data.R")
+
+source("manipulate-discharge.R")
+source("analyse-discharge.R")
+source("predict-discharge.R")
+# source("residuals-discharge.R")
+source("sensitivity-discharge.R")
+
+# source("manipulate-temperature.R")
+source("manipulate-temperature-2.R")
 source("analyse-temperature.R")
 source("predict-temperature.R")
 # source("residuals-temperature.R")
