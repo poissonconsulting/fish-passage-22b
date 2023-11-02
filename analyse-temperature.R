d7cf355f0f998fabeb1a8f401acc4b88bf9629d8
Nicole Hill <nicole@poissonconsulting.ca>
2023-11-02 15:22:27 -0700

    Simplify script

--- analyse-temperature.R
+++ analyse-temperature.R
@@ -2,15 +2,10 @@ source("header.R")
 
 sbf_set_sub("temperature")
 
+sbf_load_datas()
+sbf_load_objects()
+
 source("models-temperature.R")
-
-data <- sbf_load_data("data")
-
-D <- sbf_load_object("D") # downstream hydrologic distance matrix
-W <- sbf_load_object("W") # weighting matrix
-H <- sbf_load_object("H") # total hydrologic distance matrix
-flow_con_mat <- sbf_load_object("flow_con_mat") # flow connectivity matrix
-E <- sbf_load_object("E") # euclidean distance matrix
 
 analysis <- analyse(model, data = data, nthin = 1L)
 sbf_save_object(analysis)
