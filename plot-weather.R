366da636e1d962ecc83a5306961b69c28a4a6bb9
Nicole Hill <nicole@poissonconsulting.ca>
2023-11-02 17:07:04 -0700

    Update plots

--- plot-weather.R
+++ plot-weather.R
@@ -3,21 +3,9 @@ source("header.R")
 sbf_set_sub("query")
 sbf_load_datas()
 
-us_weather_discharge_obs %<>% 
-  group_by(station_id, date) %>% 
-  summarize(
-    us_evap_water = mean(us_evap_water),
-    us_snowmelt = mean(us_snowmelt),
-    us_runoff_surface = mean(us_runoff_surface),
-    us_precip = mean(us_precip),
-    upstream_area = first(upstream_area),
-    .groups = "drop"
-  )
-
 weather_discharge %<>% 
   group_by(station_id, date) %>% 
   summarize(
-    evap_water = mean(evap_water),
     snowmelt = mean(snowmelt),
     runoff_surface = mean(runoff_surface),
     precip = mean(precip),
@@ -72,7 +60,7 @@ sbf_print(gp)
 # Upstream weather variables through time ----
 ## Upstream Surface Runoff ----
 gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = us_runoff_surface, group = station_id, colour = station_id)) +
+  geom_line(aes(x = date, y = mean_us_runoff_surface, group = station_id, colour = station_id)) +
   xlab("Date") +
   ylab("Upstream Surface Runoff (m)") +
   NULL
@@ -82,7 +70,7 @@ sbf_print(gp)
 
 ## Upstream Precipitation ----
 gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = us_precip, group = station_id, colour = station_id)) +
+  geom_line(aes(x = date, y = mean_us_precip, group = station_id, colour = station_id)) +
   xlab("Date") +
   ylab("Upstream Precipitation (m)") +
   NULL
@@ -90,19 +78,9 @@ gp <- ggplot(discharge) +
 sbf_open_window()
 sbf_print(gp)
 
-## Upstream Evaporation ----
-gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = us_evap_water, group = station_id, colour = station_id)) +
-  xlab("Date") +
-  ylab("Upstream Evaporation (m)") +
-  NULL
-
-sbf_open_window()
-sbf_print(gp)
-
 ## Upstream Snowmelt ----
 gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = us_snowmelt, group = station_id, colour = station_id)) +
+  geom_line(aes(x = date, y = mean_us_snowmelt, group = station_id, colour = station_id)) +
   xlab("Date") +
   ylab("Upstream Snow Melt (m)") +
   NULL
@@ -111,19 +89,9 @@ sbf_open_window()
 sbf_print(gp)
 
 ## Relationship with discharge
-# Upstream Evaporation ----
-gp <- ggplot(discharge) +
-  geom_point(aes(x = us_evap_water, y = discharge, colour = station_id)) +
-  xlab("Upstream Evaporation (m)") +
-  ylab(expression("Discharge"~(m^3/s))) + 
-  NULL
-
-sbf_open_window()
-sbf_print(gp)
-
 # Upstream Snowmelt ----
 gp <- ggplot(discharge) +
-  geom_point(aes(x = us_snowmelt, y = discharge, colour = station_id)) +
+  geom_point(aes(x = mean_us_snowmelt, y = discharge, colour = station_id)) +
   xlab("Upstream Snow Melt (m)") +
   ylab(expression("Discharge"~(m^3/s))) + 
   NULL
@@ -133,7 +101,7 @@ sbf_print(gp)
 
 # Upstream Surface Runoff ----
 gp <- ggplot(discharge) +
-  geom_point(aes(x = us_runoff_surface, y = discharge, colour = station_id)) +
+  geom_point(aes(x = mean_us_runoff_surface, y = discharge, colour = station_id)) +
   xlab("Upstream Surface Runoff (m)") +
   ylab(expression("Discharge"~(m^3/s))) + 
   NULL
@@ -143,7 +111,7 @@ sbf_print(gp)
 
 # Upstream Precipitation ----
 gp <- ggplot(discharge) +
-  geom_point(aes(x = us_precip, y = discharge, colour = station_id)) +
+  geom_point(aes(x = mean_us_precip, y = discharge, colour = station_id)) +
   xlab("Upstream Precipitation (m)") +
   ylab(expression("Discharge"~(m^3/s))) + 
   NULL
@@ -155,7 +123,7 @@ sbf_print(gp)
 # Other relationships ----
 # Surface Runoff vs Precipitation ----
 gp <- ggplot(discharge) +
-  geom_point(aes(x = us_precip, y = us_runoff_surface, colour = station_id)) +
+  geom_point(aes(x = mean_us_precip, y = mean_us_runoff_surface, colour = station_id)) +
   ylab("Upstream Surface Runoff (m)") +
   xlab("Upstream Precipitation (m)") +
   NULL
