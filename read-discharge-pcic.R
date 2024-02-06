120eb123456cab7d770dcb1eaeae3be17b5223c0
almac2022 <al@newgraphenvironment.com>
2024-02-06 00:34:46 -0800

    grab individual indexes for max and min

--- read-discharge-pcic.R
+++ read-discharge-pcic.R
@@ -24,40 +24,38 @@ lat <- ncvar_get(nc_data, "lat")
 lon <- ncvar_get(nc_data, "lon")
 
 # Inspect the variables
-# print(time)
-# print(lat)
+print(time)
+print(lat)
 # print(lon)
 
 # Convert these dates into "days since 1945-1-1"
 origin <- as.Date("1945-01-01")
-dates <- seq(as_date("2019-07-13"), as_date("2021-10-29"), by = "day")
+date_start <- as.Date("2019-07-13")
+date_end <- as.Date("2021-10-29")
+# dates <- seq(as_date("2019-07-13"), as_date("2021-10-29"), by = "day")
 
-target_dates <- as.Date(dates)
-days_since_origin <- as.numeric(target_dates - origin)
+# target_dates <- as.Date(dates)
+days_ind_start <- as.numeric(date_start - origin)
+days_ind_end <- as.numeric(date_end - origin)
 
-# Now, days_since_origin contains the number of days since 1945-1-1 for each target date
-# print(days_since_origin)
-
-# Find the closest indices for your target days
-date_indices <- sapply(days_since_origin, function(day) which.min(abs(time - day)))
 
 # Define your bounding box
-target_lat_min <- 53.32
-target_lat_max <- 54.89
-target_lon_min <- -127.53
-target_lon_max <- -122.92
+lat_min <- 53.32
+lat_max <- 54.89
+lon_min <- -127.53
+lon_max <- -122.92
 
-# Find indices that match the bounding box
-lat_indices <- which(lat >= target_lat_min & lat <= target_lat_max)
-lon_indices <- which(lon >= target_lon_min & lon <= target_lon_max)
+# Find indices that are closest to your target lat/lon
+lat_min_idx <- max(lat[lat < lat_min])
+lat_max_idx <- min(lat[lat > lat_max])
+lon_min_idx <- max(lon[lon < lon_min])
+lon_max_idx <- min(lon[lon > lon_max])
 
-# print(lat_indices)
-# print(lon_indices)
 
 # Close the ncdf4 file connection
 nc_close(nc_data)
 
 
-# Using terra
+# gpt4 suggestion:  Using terra
 r <- rast(url)
 r_subset <- crop(r, ext(target_lon_min, target_lon_max, target_lat_min, target_lat_max))
