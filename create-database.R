7e744cfbfb2c4a2a9dec67d541736d57d2953644
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-04 16:14:23 -0700

    Use PCIC discharge data

--- create-database.R
+++ create-database.R
@@ -5,7 +5,7 @@ sbf_set_db_name("fish-passage-22")
 sbf_create_db()
 
 sbf_load_datas(sub = "tidy/water-temp")
-sbf_load_datas(sub = "tidy/discharge")
+sbf_load_datas(sub = "tidy/discharge2")
 sbf_load_datas(sub = "tidy/weather")
 
 sbf_execute_db("CREATE TABLE water_temp_site (
@@ -53,35 +53,6 @@ sbf_execute_db("CREATE TABLE water_temp (
                FOREIGN KEY (flag) REFERENCES water_temp_flag(flag))")
 
 sbf_save_data_to_db(water_temp)
-# 
-sbf_execute_db("CREATE TABLE discharge_site (
-               station_id TEXT NOT NULL,
-               river TEXT NOT NULL,
-               station TEXT NOT NULL,
-               altitude REAL,
-               geometry BLOB NOT NULL,
-               CHECK(
-               LENGTH(station_id) = 7 AND
-               altitude > 0 AND
-               altitude < 2500
-               ),
-               PRIMARY KEY (station_id))")
-
-sbf_save_data_to_db(discharge_site)
-
-sbf_execute_db("CREATE TABLE discharge (
-               station_id TEXT NOT NULL,
-               date TEXT NOT NULL,
-               discharge REAL NOT NULL,
-               CHECK(
-               DATE(date) IS date AND
-               discharge > 0 AND
-               discharge < 1000
-               ),
-               PRIMARY KEY (station_id, date),
-               FOREIGN KEY (station_id) REFERENCES discharge_site(station_id))")
-
-sbf_save_data_to_db(discharge)
 
 sbf_execute_db("CREATE TABLE weather_site (
                cell_id TEXT NOT NULL,
@@ -122,72 +93,30 @@ sbf_execute_db("CREATE TABLE weather_temp (
 
 sbf_save_data_to_db(weather_temp)
 
-sbf_execute_db("CREATE TABLE weather_discharge (
-               cell_id TEXT NOT NULL,
-               station_id TEXT NOT NULL,
+sbf_execute_db("CREATE TABLE discharge_site (
+               discharge_site TEXT NOT NULL,
+               geometry GLOB NOT NULL,
+               PRIMARY KEY (discharge_site))")
+
+sbf_save_data_to_db(discharge_site)
+
+sbf_execute_db("CREATE TABLE discharge (
+               latitude REAL NOT NULL,
+               longitude REAL NOT NULL,
+               discharge_site TEXT NOT NULL,
                date TEXT NOT NULL,
-               time TEXT NOT NULL,
-               air_temp REAL NOT NULL,
-               evap_water REAL NOT NULL,
-               evap_total REAL NOT NULL,
-               snowmelt REAL NOT NULL,
-               runoff_subsurface REAL NOT NULL,
-               runoff_surface REAL NOT NULL,
-               net_solar_rad REAL NOT NULL,
-               precip REAL NOT NULL,
-               soil_vol_1 REAL NOT NULL,
+               discharge REAL NOT NULL,
                CHECK(
                DATE(date) IS date AND
-               time == STRFTIME('%H:%M:%S', time) AND
-               air_temp > -50 AND
-               air_temp < 50 AND
-               snowmelt >= 0 AND
-               runoff_subsurface >= 0 AND
-               runoff_surface >= 0 AND
-               net_solar_rad >= 0 AND
-               precip >= 0 AND 
-               soil_vol_1 > 0 
+               discharge >= 0 AND
+               discharge <= 1e12 AND
+               latitude <= 56 AND
+               latitude >= 50 AND
+               longitude >= -128 AND
+               longitude <= -121
                ),
-               PRIMARY KEY (cell_id, station_id, date, time),
-               FOREIGN KEY (cell_id) REFERENCES weather_site(cell_id),
-               FOREIGN KEY (station_id) references discharge_site(station_id))")
+               PRIMARY KEY (latitude, longitude, date),
+               FOREIGN KEY (discharge_site) REFERENCES discharge_site(discharge_site)
+               )")
 
-sbf_save_data_to_db(weather_discharge)
-
-sbf_execute_db("CREATE TABLE us_weather_discharge_obs (
-               station_id TEXT NOT NULL,
-               date TEXT NOT NULL,
-               mean_us_snowmelt REAL NOT NULL,
-               mean_us_runoff_surface REAL NOT NULL,
-               mean_us_precip REAL NOT NULL,
-               upstream_area REAL NOT NULL,
-               CHECK(
-               DATE(date) IS date AND
-               mean_us_snowmelt >= 0 AND
-               mean_us_runoff_surface >= 0 AND
-               mean_us_precip >= 0 AND 
-               upstream_area > 1000
-               ),
-               PRIMARY KEY (station_id, date),
-               FOREIGN KEY (station_id) REFERENCES discharge_site(station_id))")
-
-sbf_save_data_to_db(us_weather_discharge_obs)
-
-sbf_execute_db("CREATE TABLE us_weather_discharge_pred (
-               site TEXT NOT NULL,
-               date TEXT NOT NULL,
-               mean_us_snowmelt REAL NOT NULL,
-               mean_us_runoff_surface REAL NOT NULL,
-               mean_us_precip REAL NOT NULL,
-               upstream_area REAL NOT NULL,
-               CHECK(
-               DATE(date) IS date AND
-               mean_us_snowmelt >= 0 AND
-               mean_us_runoff_surface >= 0 AND
-               mean_us_precip >= 0 AND 
-               upstream_area > 1000
-               ),
-               PRIMARY KEY (site, date),
-               FOREIGN KEY (site) REFERENCES water_temp_site(site))")
-
-sbf_save_data_to_db(us_weather_discharge_pred)
+sbf_save_data_to_db(discharge)
