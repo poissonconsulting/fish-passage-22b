b12dbef6819d44d26f7456a09c3ef2150ecfaf93
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-04 16:18:40 -0700

    Changes to discharge plots

--- plot-discharge.R
+++ plot-discharge.R
@@ -3,19 +3,26 @@ source("header.R")
 sbf_set_sub("query")
 sbf_load_datas()
 
+discharge %<>% 
+  group_by(latitude, longitude) %>% 
+  mutate(
+    station_id = cur_group_id(),
+    station_id = factor(station_id)
+  )
+
 gp <- ggplot(discharge) +
-  geom_line(aes(x = date, y = discharge, colour = station_id, group = station_id)) +
-  scale_colour_disc_poisson() +
+  geom_line(aes(x = date, y = discharge, colour = station_id, group = station_id), alpha = 0.2) +
   ylab(expression("Discharge"~(m^3/s))) +
   xlab("Date") +
   labs(colour = "Station ID") +
+  theme(legend.position = "none") +
   NULL
 
-sbf_open_window(6, 4)
+sbf_open_window(8, 10)
 sbf_print(gp)
 
 sbf_save_plot(
   x_name = "discharge",
   report = FALSE,
-  caption = "Mean daily discharge (cubic metres per second) by date and site"
+  caption = "Mean daily discharge by date and site"
 )
