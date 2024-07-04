9c3eecccda6d9bd16748fdcf4b0a97baf33b1860
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-04 16:18:56 -0700

    Plot relationships of key variables with water temp

---
+++ plot-relationships.R
@@ -0,0 +1,129 @@
+source("header.R")
+
+sbf_set_sub("temperature")
+sbf_load_datas()
+
+sbf_set_sub("plot-relationships")
+
+data %<>% 
+  mutate(dayte = dtt_dayte(date))
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = net_solar_rad, y = air_temp, colour = as.numeric(week))) +
+  xlab("Net Solar Radiation") +
+  ylab("Air Temperature") +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = net_solar_rad, y = discharge, colour = water_temp)) +
+  xlab("Net Solar Radiation") +
+  ylab("Discharge") +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data) +
+  geom_line(aes(x = dayte, y = water_temp, colour = site)) +
+  xlab("Day") +
+  ylab("Water Temp") +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = dayte, y = net_solar_rad, colour = discharge)) +
+  facet_wrap(~site) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_line(aes(x = dayte, y = water_temp, colour = elev)) +
+  facet_wrap(~site) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = net_solar_rad, y = air_temp, colour = water_temp)) +
+  facet_wrap(~annual) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = air_temp, y = water_temp, colour = discharge)) +
+  facet_wrap(~annual) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = dayte, y = air_temp, colour = water_temp)) +
+  facet_wrap(~site) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+gp <- ggplot(data %>% filter(!is.na(water_temp))) +
+  geom_point(aes(x = dayte, y = discharge, colour = water_temp)) +
+  facet_wrap(~site) +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
+data %>% 
+  group_by(site) %>% 
+  summarize(
+    elev = first(elev),
+    discharge = mean(discharge, na.rm = TRUE),
+    water_temp = mean(water_temp, na.rm = TRUE),
+    air_temp = mean(air_temp, na.rm = TRUE),
+    net_solar_rad = mean(net_solar_rad, na.rm = TRUE)
+  ) %>% 
+  mutate(
+    across(
+      c(elev, discharge, water_temp, air_temp, net_solar_rad),
+      \(x) scale(x)       
+    )
+  ) %>% 
+  print(n = nrow(.))
+
+
+data %<>% 
+  pivot_longer(
+    cols = c(
+      air_temp,
+      net_solar_rad,
+      discharge,
+      elev
+    ),
+    names_to = "variable",
+    values_to = "value"
+  ) %>% 
+  select(site, week, annual, variable, value, water_temp, dayte) %>% 
+  mutate(
+    week = as.numeric(as.character(week))
+  )
+
+gp <- ggplot(data) +
+  geom_point(aes(x = value, y = water_temp, colour = dayte, shape = factor(annual)), alpha = 0.4) +
+  facet_grid(cols = vars(variable), rows = vars(site), scales = "free_x") +
+  xlab("Value") +
+  ylab("Water Temp") +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+ 
