93f68f4d5f1830434bae25994d6f05c0332b6d4f
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-04 16:17:08 -0700

    Change params to monitor when troubleshooting divergent transitions

--- pairs-stanfit.R
+++ pairs-stanfit.R
@@ -7,7 +7,7 @@ fit <- analysis$stanfit
 
 sbf_open_window(16, 10)
 
-pairs(fit, pars = c("alpha_tu", "alpha_td", "sigma_tu", "sigma_td", "phi", "bIntercept", "bDoy", "bDoy2", "var_nug", "lp__"))
+pairs(fit, pars = c("alpha_td", "sigma_td", "alpha_tu", "sigma_tu", "alpha_ed", "sigma_ed", "bIntercept", "bDoy", "bDoy2", "var_nug", "lp__"))
 
 
 # SSN bayes example
