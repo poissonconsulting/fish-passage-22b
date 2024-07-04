a2613cd33396098fb89d2f1cdb60edfc089af6d4
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-04 16:03:43 -0700

    Update run-all with all models

--- zzz-run-all.R
+++ zzz-run-all.R
@@ -10,13 +10,21 @@ if(FALSE) {
 source("read-flow-diagrams.R")
 source("read-dags.R")
 
+if (FALSE) {
+  # Downloads discharge data from PCIC to dropbox Data folder
+  source("download-discharge-pcic.R")
+  # Downloads air temperature from ERA5 dataset (requires API key)
+  # Not sure where will save it yet.
+  source("download-air-temp.R") # not sure if this works yet
+}
+
 source("read-water-temp.R")
 source("clean-water-temp.R")
 source("tidy-water-temp.R")
 
-source("read-discharge.R")
-source("clean-discharge.R")
-source("tidy-discharge.R")
+source("read-discharge2.R")
+source("clean-discharge2.R")
+source("tidy-discharge2.R")
 
 source("read-weather.R")
 source("clean-weather.R")
@@ -27,22 +35,55 @@ source("query-database.R")
 
 source("plot-water-temp.R")
 source("plot-discharge.R")
-source("plot-weather.R")
 
 source("SSN-distances.R")
 
 source("prepare-data.R")
 
-source("manipulate-discharge.R")
-source("analyse-discharge.R")
-source("predict-discharge.R")
-# source("residuals-discharge.R")
-source("sensitivity-discharge.R")
-
 source("manipulate-temperature.R")
-source("analyse-temperature.R")
-source("predict-temperature.R")
-# source("residuals-temperature.R")
+source("plot-relationships.R")
+
+# Temperature models ----
+source("analyse-temperature-glm.R")
+source("predict-temperature-glm.R")
+
+source("analyse-temperature-logistic.R") # site-wise phi
+source("predict-temperature-logistic.R") # site-wise phi
+
+source("analyse-temperature-logistic-fixed-phi.R")
+source("predict-temperature-logistic-fixed-phi.R")
+
+source("analyse-temperature-logistic-fixed-phi-re-mu-gamma.R")
+source("predict-temperature-logistic-fixed-phi-re-mu-gamma.R")
+
+source("analyse-temperature-logistic-fixed-phi-re-alpha-beta-gamma.R")
+source("predict-temperature-logistic-fixed-phi-re-alpha-beta-gamma.R")
+
+source("analyse-temperature-logistic-no-phi.R")
+source("predict-temperature-logistic-no-phi.R")
+
+source("analyse-temperature-logistic-no-phi-re-gamma.R")
+source("predict-temperature-logistic-no-phi-re-gamma.R")
+
+source("analyse-temperature-logistic-no-phi-re-alpha-beta.R")
+source("predict-temperature-logistic-no-phi-re-alpha-beta.R")
+
+source("analyse-temperature-logistic-no-phi-re-alpha-beta-gamma.R")
+source("predict-temperature-logistic-no-phi-re-alpha-beta-gamma.R")
+
+source("compare-acf.R")
+
+source("test-temperature-heat.R")
+source("analyse-temperature-heat.R")
+source("predict-temperature-heat.R")
+
+source("analyse-temperature-rate.R")
+source("predict-temperature-rate.R")
+
+source("compare-temperature.R")
+
+source("predict-gsdd.R")
+
 source("sensitivity-temperature.R")
 
 if(FALSE) {
