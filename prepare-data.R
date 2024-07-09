4977a5577edf5fb69906cd171a62da6c21acd1d9
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-08 17:14:17 -0700

    Update with new discharge data

--- prepare-data.R
+++ prepare-data.R
@@ -6,36 +6,39 @@ sbf_load_datas()
 D <- sbf_load_object("downstream_hydrologic_distance", sub = "distance/temp")
 dist_sites <- rownames(D)
 
-# Keep all water temp except for "fail"
-water_temp %<>%
-  filter(flag != "F") %>% 
+# Sites
+sites <- 
+  water_temp_site %>% 
+  tibble() %>% 
+  select(site)
+
+# Date ref for bounding dates
+dates_sites_all <-
+  tibble(
+    date = seq(bounding_dates[1], bounding_dates[2], by = "day")
+  ) %>% 
   mutate(
+    annual = dtt_year(date),
     day = as.integer(date),
-    day = day - min(day),
+    day = day - min(day) + 1,
     week = day %/% 7 + 1,
-    annual = dtt_year(date)
-  )
+  ) %>% 
+  cross_join(sites)
 
-date_ref_full <- 
-  water_temp %>% 
-  distinct(site, date, week, annual)
-
-date_ref <-
-  date_ref_full %>% 
-  add_count(week) %>% 
-  filter(n > 21) %>% 
-  distinct(date, week, annual) %>% 
-  arrange(date)
-
-date_ref_summ <- 
-  date_ref %>% 
-  arrange(date) %>% 
+first_date_week <- 
+  dates_sites_all %>% 
+  distinct(date, week) %>% 
   group_by(week) %>% 
   slice_head() %>% 
   ungroup()
 
+# Keep all water temp except for "fail"
+water_temp %<>%
+  filter(flag != "F") %>% 
+  full_join(dates_sites_all, join_by(date, site))
+
 water_temp %<>% 
-  filter(week %in% date_ref$week) %>% 
+  # filter(week %in% date_ref$week) %>% 
   group_by(site, week) %>% 
   summarize(
     water_temp = mean(temp),
@@ -44,17 +47,12 @@ water_temp %<>%
   ) %>% 
   arrange(site, week)
 
-# Aggregate weather data by week
-weather_temp %<>% 
-  filter(date %in% seq(min(date_ref$date), max(date_ref$date), by = "day")) %>%
-  left_join(date_ref, join_by(date)) %>%
+# Aggregate air temp data by week
+air_temp %<>%
+  full_join(dates_sites_all, join_by(site, date)) %>%
   group_by(site, week) %>% 
   summarize(
-    across(
-      c("air_temp", "evap_water", "evap_total", "snowmelt", "runoff_subsurface",
-        "runoff_surface", "net_solar_rad", "precip", "soil_vol_1"),
-      \(x) mean(x)
-    ),
+    air_temp = mean(air_temp),
     .groups = "drop"
   ) %>% 
   arrange(site, week)
@@ -71,8 +69,7 @@ closest_sites <-
 
 discharge %<>% 
   right_join(closest_sites, join_by(discharge_site), relationship = "many-to-many") %>% 
-  filter(date %in% seq(min(date_ref$date), max(date_ref$date), by = "day")) %>%
-  left_join(date_ref, join_by(date)) %>%
+  left_join(dates_sites_all, join_by(date, site)) %>%
   group_by(discharge_site, week, site) %>% 
   summarize(
     discharge = mean(discharge),
@@ -87,12 +84,10 @@ elev <-
 
 # Add missing values for water temp by expanding to every site-week combo
 water_temp %<>% 
-  expand(site, week = full_seq(week, 1)) %>% 
-  left_join(water_temp %>% select(-annual), join_by(site, week)) %>% 
-  left_join(date_ref_summ, join_by(week)) %>% 
-  left_join(weather_temp, join_by(site, week)) %>% 
+  left_join(air_temp, join_by(site, week)) %>% 
   left_join(discharge, join_by(site, week)) %>% 
   left_join(elev, join_by(site)) %>% 
+  left_join(first_date_week, join_by(week)) %>% 
   mutate(
     site_remains = if_else2(site %in% dist_sites, TRUE, FALSE)
   ) %>% 
@@ -101,7 +96,7 @@ water_temp %<>%
     site = factor(site),
     week = factor(week)
   ) %>%
-  ### Must be arranged in this order for the model
+  ### **Must be arranged in this order for the model**
   arrange(week, site)
 
 sbf_set_sub("prepare")
