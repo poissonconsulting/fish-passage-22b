4a5985db2b5a878f2ddc1c18ef815eb5e1b2a0d2
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-08 17:23:06 -0700

    Update runall

--- zzz-run-all.R
+++ zzz-run-all.R
@@ -23,7 +23,6 @@ if (download) {
   source("download-air-temp.R") 
 }
 
-# Make sure the download T/F above will trigger the following scripts to figure themselves out automatically.
 source("read-water-temp.R")
 source("clean-water-temp.R")
 source("tidy-water-temp.R")
@@ -32,15 +31,9 @@ source("read-air-temp.R")
 source("clean-air-temp.R")
 source("tidy-air-temp.R")
 
-source("read-discharge2.R") # Change names to just discharge
-source("clean-discharge2.R")
-source("tidy-discharge2.R")
-
-# if (FALSE) { # don't really need these - just for air temp!
-#   source("read-weather.R")
-#   source("clean-weather.R")
-#   source("tidy-weather.R")
-# }
+source("read-discharge.R")
+source("clean-discharge.R")
+source("tidy-discharge.R")
 
 source("create-database.R")
 source("query-database.R")
@@ -54,59 +47,12 @@ source("SSN-distances-temp.R")
 source("prepare-data.R")
 
 source("manipulate-temperature.R")
-# source("plot-relationships.R") # save plots with report = FALSE/get rid of old vars
-
-# Temperature models ----
-
-## Delete old models; remove other covariance structures.
-## Delete other old files!!
-## Make sure repo runs through in full over the weekend
-## Report!
-## Copy air temperature data to Al's dropbox
-
-# source("analyse-temperature-glm.R")
-# source("predict-temperature-glm.R")
-# 
-# source("analyse-temperature-logistic.R") # site-wise phi
-# source("predict-temperature-logistic.R") # site-wise phi
-# 
-# source("analyse-temperature-logistic-fixed-phi.R")
-# source("predict-temperature-logistic-fixed-phi.R")
-# 
-# source("analyse-temperature-logistic-fixed-phi-re-mu-gamma.R")
-# source("predict-temperature-logistic-fixed-phi-re-mu-gamma.R")
-# 
-# source("analyse-temperature-logistic-fixed-phi-re-alpha-beta-gamma.R")
-# source("predict-temperature-logistic-fixed-phi-re-alpha-beta-gamma.R")
-# 
-# source("analyse-temperature-logistic-no-phi.R")
-# source("predict-temperature-logistic-no-phi.R")
-# 
-# source("analyse-temperature-logistic-no-phi-re-gamma.R")
-# source("predict-temperature-logistic-no-phi-re-gamma.R")
-# 
-# source("analyse-temperature-logistic-no-phi-re-alpha-beta.R")
-# source("predict-temperature-logistic-no-phi-re-alpha-beta.R")
-
-# source("analyse-temperature-logistic-no-phi-re-alpha-beta-gamma.R")
-# source("predict-temperature-logistic-no-phi-re-alpha-beta-gamma.R")
-
 source("analyse-temperature.R") 
 source("predict-temperature.R")
+source("residuals-temperature.R")
 source("sensitivity-temperature.R")
 
 source("predict-gsdd.R")
-
-# source("compare-acf.R")
-
-# source("test-temperature-heat.R")
-# source("analyse-temperature-heat.R")
-# source("predict-temperature-heat.R")
-# 
-# source("analyse-temperature-rate.R")
-# source("predict-temperature-rate.R")
-
-# source("compare-temperature.R")
 
 if(FALSE) {
   source("knit-report.R")
