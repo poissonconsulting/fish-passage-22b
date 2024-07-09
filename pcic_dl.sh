1a22727cbf0b636ea320aa30963c6918865569a0
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-08 17:12:46 -0700

    Update .sh file for discharge download

--- pcic_dl.sh
+++ pcic_dl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+export PATH=/opt/homebrew/bin:$PATH
 
-curl -o '/Users/airvine/Dropbox/New Graph/fish-passage-22/Data/Discharge/pcic/baseflow.nc' 'https://data.pacificclimate.org/data/hydro_model_out/allwsbc.ACCESS1-0_rcp85_r1i1p1.1945to2099.BASEFLOW.nc.nc?BASEFLOW'$(echo '[27221:28060][196:222][200:274]'|jq -sRr @uri)
 
-curl -o '/Users/airvine/Dropbox/New Graph/fish-passage-22/Data/Discharge/pcic/runoff.nc' 'https://data.pacificclimate.org/data/hydro_model_out/allwsbc.ACCESS1-0_rcp85_r1i1p1.1945to2099.RUNOFF.nc.nc?RUNOFF'$(echo '[27221:28060][196:222][200:274]'|jq -sRr @uri)
+curl -o '/Users/nicolehill/Poisson/Data/fish-passage/2022/Data/Discharge/pcic/baseflow.nc' 'https://data.pacificclimate.org/data/hydro_model_out/allwsbc.ACCESS1-0_rcp85_r1i1p1.1945to2099.BASEFLOW.nc.nc?BASEFLOW'$(echo '[27028:28123][196:222][200:274]'|jq -sRr @uri)
+
+curl -o '/Users/nicolehill/Poisson/Data/fish-passage/2022/Data/Discharge/pcic/runoff.nc' 'https://data.pacificclimate.org/data/hydro_model_out/allwsbc.ACCESS1-0_rcp85_r1i1p1.1945to2099.RUNOFF.nc.nc?RUNOFF'$(echo '[27028:28123][196:222][200:274]'|jq -sRr @uri)
