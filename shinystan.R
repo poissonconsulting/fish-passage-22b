11639810169980104f1619d1f2da7c313575146b
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-08 17:16:18 -0700

    Delete old scripts

--- shinystan.R
+++
@@ -1,9 +0,0 @@
-source("header.R")
-
-sbf_set_sub("temperature")
-analysis <- sbf_load_object("analysis")
-
-install.packages("shinystan")
-library(shinystan)
-fit <- analysis$stanfit
-launch_shinystan(fit)
