40afe14910f5ae4daa1c484a563e5aeb602b9a67
Nicole Hill <nicole@poissonconsulting.ca>
2024-07-11 10:44:00 -0700

    Add new graph directory

--- knit-report.R
+++ knit-report.R
@@ -1,8 +1,10 @@
 source("header.R")
 
 poisreport::knit_report("fish-passage-22.Rmd", ask = FALSE)
-
 poisreport::report_to_directory()
+
+dir <- "~/Poisson/Clients - Transfer/New Graph/"
+poisreport::report_to_directory(dir = dir)
 
 if(FALSE) {
   if (require(poisblogdown)) poisblogdown::report_to_blogdown()
