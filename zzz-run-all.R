f071bf571a10f810a3fd52d5fe095060da3cfa02
Nicole Hill <nicole@poissonconsulting.ca>
2024-09-25 14:57:02 -0700

    Update run-all

--- zzz-run-all.R
+++ zzz-run-all.R
@@ -18,9 +18,10 @@ if (download) {
   source("download-water-temp.R")
   # Downloads discharge data from PCIC to dropbox Data folder
   source("download-discharge-pcic.R")
-  # Downloads air temperature from ERA5 dataset (requires API key)
+  # Downloads air temperature and net solar radiation from ERA5 dataset (requires API key)
   # follow directions from https://github.com/joaohenry23/Download_ERA5_with_python
-  source("download-air-temp.R") 
+  source("download-air-temp.R")
+  source("download-solar-rad.R")
 }
 
 source("read-water-temp.R")
@@ -34,6 +35,10 @@ source("tidy-air-temp.R")
 source("read-discharge.R")
 source("clean-discharge.R")
 source("tidy-discharge.R")
+
+source("read-solar-rad.R")
+source("clean-solar-rad.R")
+source("tidy-solar-rad.R")
 
 source("create-database.R")
 source("query-database.R")
