3737a72bb382194a8e48488fc23c8667bf7bb91b
Nicole Hill <nicole@poissonconsulting.ca>
2024-09-25 14:56:27 -0700

    Download script for net solar radiation

---
+++ download-solar-rad.R
@@ -0,0 +1,13 @@
+source("header.R")
+
+message("Need API for Climate Data Store - follow directions from https://github.com/joaohenry23/Download_ERA5_with_python")
+message("This could take a while to download")
+
+sbf_set_sub("solar-rad")
+x <- tibble(x = 0)
+sbf_save_data(x)
+sbf_reset_sub()
+
+reticulate::use_virtualenv("r-reticulate")
+cdsapi <- reticulate::import("cdsapi")
+reticulate::source_python("download-solar-rad.py")
