b9f9b626561412a2e027e7de8a9ea6df8fa5a590
Nicole Hill <nicole@poissonconsulting.ca>
2024-10-21 08:43:11 -0700

    Update query tables

--- query-database.R
+++ query-database.R
@@ -3,8 +3,12 @@ source("header.R")
 sbf_load_datas_from_db()
 
 water_temp <- drop_units(water_temp)
+water_temp_site <- drop_units(water_temp_site)
 discharge <- drop_units(discharge)
 air_temp <- drop_units(air_temp)
+solar_rad <- drop_units(solar_rad)
+dewpoint_temp <- drop_units(dewpoint_temp)
+surface_pressure <- drop_units(surface_pressure)
 
 sbf_set_sub("query")
 sbf_save_datas()
