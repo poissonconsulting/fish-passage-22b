03c29764cdc993ec72724293fdeb021e668af0bd
Nicole Hill <nicole@poissonconsulting.ca>
2024-10-21 08:39:58 -0700

    Add new env data to db, update discharge table

--- create-database.R
+++ create-database.R
@@ -7,6 +7,9 @@ sbf_create_db()
 sbf_load_datas(sub = "tidy/water-temp")
 sbf_load_datas(sub = "tidy/discharge")
 sbf_load_datas(sub = "tidy/air-temp")
+sbf_load_datas(sub = "tidy/solar-rad")
+sbf_load_datas(sub = "tidy/dewpoint-temp")
+sbf_load_datas(sub = "tidy/surface-pressure")
 
 sbf_execute_db("CREATE TABLE water_temp_site (
                site TEXT NOT NULL,
@@ -79,30 +82,92 @@ sbf_execute_db("CREATE TABLE air_temp (
 
 sbf_save_data_to_db(air_temp)
 
-sbf_execute_db("CREATE TABLE discharge_site (
-               discharge_site TEXT NOT NULL,
-               geometry GLOB NOT NULL,
-               PRIMARY KEY (discharge_site))")
+sbf_execute_db("CREATE TABLE solar_rad_site (
+               cell_id TEXT NOT NULL,
+               geometry BLOB NOT NULL,
+               PRIMARY KEY(cell_id))")
 
-sbf_save_data_to_db(discharge_site)
+sbf_save_data_to_db(solar_rad_site)
+
+sbf_execute_db("CREATE TABLE solar_rad (
+               cell_id TEXT NOT NULL,
+               site TEXT NOT NULL,
+               date TEXT NOT NULL,
+               time TEXT NOT NULL,
+               solar_rad REAL NOT NULL,
+               CHECK(
+               DATE(date) IS date AND
+               time == STRFTIME('%H:%M:%S', time) AND
+               solar_rad >= 0 AND
+               solar_rad < 5e7
+               ),
+               PRIMARY KEY (cell_id, site, date, time),
+               FOREIGN KEY (cell_id) REFERENCES solar_rad_site(cell_id),
+               FOREIGN KEY (site) references water_temp_site(site))")
+
+sbf_save_data_to_db(solar_rad)
+
+sbf_execute_db("CREATE TABLE dewpoint_temp_site (
+               cell_id TEXT NOT NULL,
+               geometry BLOB NOT NULL,
+               PRIMARY KEY(cell_id))")
+
+sbf_save_data_to_db(dewpoint_temp_site)
+
+sbf_execute_db("CREATE TABLE dewpoint_temp (
+               cell_id TEXT NOT NULL,
+               site TEXT NOT NULL,
+               date TEXT NOT NULL,
+               time TEXT NOT NULL,
+               dewpoint_temp REAL NOT NULL,
+               CHECK(
+               DATE(date) IS date AND
+               time == STRFTIME('%H:%M:%S', time) AND
+               dewpoint_temp >= -50 AND
+               dewpoint_temp < 40
+               ),
+               PRIMARY KEY (cell_id, site, date, time),
+               FOREIGN KEY (cell_id) REFERENCES dewpoint_temp_site(cell_id),
+               FOREIGN KEY (site) references water_temp_site(site))")
+
+sbf_save_data_to_db(dewpoint_temp)
+
+sbf_execute_db("CREATE TABLE surface_pressure_site (
+               cell_id TEXT NOT NULL,
+               geometry BLOB NOT NULL,
+               PRIMARY KEY(cell_id))")
+
+sbf_save_data_to_db(surface_pressure_site)
+
+sbf_execute_db("CREATE TABLE surface_pressure (
+               cell_id TEXT NOT NULL,
+               site TEXT NOT NULL,
+               date TEXT NOT NULL,
+               time TEXT NOT NULL,
+               surface_pressure REAL NOT NULL,
+               CHECK(
+               DATE(date) IS date AND
+               time == STRFTIME('%H:%M:%S', time) AND
+               surface_pressure >= 1e4 AND
+               surface_pressure < 1e6
+               ),
+               PRIMARY KEY (cell_id, site, date, time),
+               FOREIGN KEY (cell_id) REFERENCES surface_pressure_site(cell_id),
+               FOREIGN KEY (site) references water_temp_site(site))")
+
+sbf_save_data_to_db(surface_pressure)
 
 sbf_execute_db("CREATE TABLE discharge (
-               latitude REAL NOT NULL,
-               longitude REAL NOT NULL,
-               discharge_site TEXT NOT NULL,
+               site TEXT NOT NULL,
                date TEXT NOT NULL,
                discharge REAL NOT NULL,
                CHECK(
                DATE(date) IS date AND
                discharge >= 0 AND
-               discharge <= 1e12 AND
-               latitude <= 56 AND
-               latitude >= 50 AND
-               longitude >= -128 AND
-               longitude <= -121
+               discharge <= 2000
                ),
-               PRIMARY KEY (latitude, longitude, date),
-               FOREIGN KEY (discharge_site) REFERENCES discharge_site(discharge_site)
+               PRIMARY KEY (site, date),
+               FOREIGN KEY (site) REFERENCES water_temp_site(site)
                )")
 
 sbf_save_data_to_db(discharge)
