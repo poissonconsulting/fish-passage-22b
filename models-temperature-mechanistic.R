d49cfd97ea01c0dad3014320736b59ea7deb447c
Nicole Hill <nicole@poissonconsulting.ca>
2024-10-21 08:43:41 -0700

    Mechanistic temperature model (not converging)

---
+++ models-temperature-mechanistic.R
@@ -0,0 +1,119 @@
+description <- c(
+  "`eAlpha`" = "Maximum stream temperature",
+  "`bAlphaIntercept`" = "Intercept for `eAlpha`",
+  "`bLogisticPars[, 1]`" = "Effect of `site` on `bAlphaIntercept`",
+  "`eGamma`" = "Measure of the steepest slope of the function",
+  "`bGammaIntercept`" = "Intercept for `eGamma`",
+  "`bLogisticPars[, 3]`" = "Effect of `site` on `bGammaIntercept`",
+  "`eBeta`" = "Air temperature at the inflection point of the curve",
+  "`bBetaIntercept`" = "Intercept for `eBeta`",
+  "`bLogisticPars[, 2]`" = "Effect of `site` on `bBetaIntercept`",
+  "`eTemp[i]`" = "Expected value of `water_temp[i]`",
+  "`y[i]`" = "The `i`^th^ water temperature value",
+  "`air_temp[i]`" = "The `i`^th^ air temperature value",
+  "`site[i]`" = "The `i`^th^ site",
+  "`Y[t]`" = "Vector of water temperature values for all sites in the `t`^th^ week",
+  "`mu[t]`" = "Vector of `eTemp` values for all sites in the `t`^th^ week",
+  "`sigma_nug`" = "Standard deviation of the nugget effect",
+  "`I`" = "The identity matrix",
+  "`var_nug`" = "Variance of the nugget effect",
+  "`var_td`" = "Variance of the tail-down covariance",
+  "`sigma_td`" = "Standard deviation of the exponential tail-down covariance model",
+  "`alpha_td`" = "The variance of spatially independent points",
+  "`bRhoLogisticPars[1,2]`" = "Correlation coefficient between the alpha and beta parameters",
+  "`bRhoLogisticPars[1,3]`" = "Correlation coefficient between the alpha and gamma parameters",
+  "`bRhoLogisticPars[2,3]`" = "Correlation coefficient between the beta and gamma parameters",
+  "`C_td`" = "Covariance matrix of the tail-down exponential model",
+  "`H`" = "Total hydrologic distance matrix",
+  "`D`" = "Downstream hydrologic distance matrix",
+  "`flow_con_mat`" = "Site connectivity matrix",
+  "`y_obs`" = "Vector of observed water temperature values",
+  "`y_mis`" = "Vector of missing water temperature values",
+  "`N_y_obs`" = "Number of observed water temperature values",
+  "`N_y_mis`" = "Number of missing water temperature values",
+  "`i_y_obs`" = "Indexes of observed water temperature values",
+  "`i_y_mis`" = "Indexes of missing water temperature values",
+  "`sLogisticPars[1]`" = "Standard deviation of the random effect of site on `eAlpha`",
+  "`sLogisticPars[2]`" = "Standard deviation of the random effect of site on `eBeta`",
+  "`sLogisticPars[3]`" = "Standard deviation of the random effect of site on `eGamma`",
+  "`nsite`" = "Number of sites",
+  "`nweek`" = "Number of weeks",
+  "`bLRhoLogisticPars`" = "Cholesky factor of the correlation matrix for the logistic curve parameters, for efficient sampling",
+  "`eZLogisticPars`" = "Matrix with standard normal priors, for efficient sampling"
+)
+
+description <- tibble(
+  Parameter = names(description),
+  Description = description
+)
+
+description %<>% arrange(Parameter)
+
+sbf_save_table(description, caption = "Parameter descriptions.")
+
+model <- model(
+  code = read_file("temperature-mechanistic.stan"),
+  modify_data = function(data) {
+    data$i_y_obs <- which(!is.na(data$water_temp))
+    data$i_y_mis <- which(is.na(data$water_temp))
+    data$N_y_obs <- length(data$i_y_obs)
+    data$N_y_mis <- length(data$i_y_mis)
+    data$y_obs <- data$water_temp[!is.na(data$water_temp)]
+    
+    data$I <- diag(1, nrow = data$nsite, ncol = data$nsite)
+    data$D <- D
+    data$W <- W
+    data$H <- H
+    data$flow_con_mat <- flow_con_mat
+    data$E <- E
+    data
+  },
+  new_expr = "
+    for (i in 1:nObs) {
+      if (week[i] == 1) {
+        eTemp[i] = bInitialTemp
+        eIceReserve[i] = bInitialIceReserve
+      } else {
+        eShortwave[i] = bShortwave * solar_rad[i];
+        eLongwave[i] = bLongwave * longwave_rad[i];
+        eDischarge[i] = bDischarge * 41.8e6 * discharge[i];
+        eHeatLoss[i] = bHeatLoss * 0.004727079 * (eTemp[i - nsite] + 273.15)^4;
+        eTempDiff[i] = (eShortwave[i] + eLongwave[i] - eHeatLoss[i]) / eDischarge[i];
+        if ((eTemp[i - nsite] + eTempDiff[i]) < 0) { 
+          eIceReserve[i] = eIceReserve[i - nsite] + eTempDiff[i];
+          eTemp[i] = eTemp[i - nsite];
+        } else {
+          if (eIceReserve[i - nsite] > 0) {
+            eIceReserve[i] = eIceReserve[i - nsite] - eTempDiff[i];
+            eTemp[i] = eTemp[i - nsite];
+          } else {
+            eIceReserve[i] = 0.0
+            eTemp[i] = eTemp[i - nsite] + eTempDiff[i];
+          }
+        }    
+      }
+      eTD[i] <- sigma_td^2 * exp(-3 * H[i] / alpha_td) # Flow-unconnected just sums distance to common confluence
+      fit[i] <- eTemp[i]
+    }",
+  new_expr_vec = FALSE,
+  gen_inits = function(data) {
+    inits <- list()
+    inits$bHeatLoss <- 1
+    inits$bDischarge <- 1
+    inits$bShortwave <- 1
+    inits$bLongwave <- 1
+    inits
+  },
+  select_data = list(
+    site = factor(),
+    week = factor(),
+    water_temp = c(-2, 30, NA),
+    solar_rad = c(0, 5e9),
+    discharge = c(0, 2000),
+    longwave_rad = c(1e6, 1e9),
+    H = c(0, 50 * 5000),
+    E = c(0, 50 * 500)
+  )
+)
+
+sbf_save_block(template(model), "template", caption = "Model description.")
---
+++ predict-temperature-mechanistic.R
@@ -0,0 +1,87 @@
+source("header.R")
+
+options(mb.parallel = FALSE)
+
+sbf_set_sub("temperature-mechanistic")
+
+analysis <- sbf_load_object("analysis")
+data <- data_set(analysis)
+
+glance <- glance(analysis)
+coef <- coef(analysis, include_constant = FALSE, simplify = TRUE) %>%
+  mutate(across(estimate:svalue, \(x) signif(x, 3))) %>% 
+  filter(!(str_detect(term, "y_mis")))
+
+glance %>% print()
+div <- partition_div(analysis$stanfit)
+print(paste("There were", nrow(div[[1]]), "divergent transitions"))
+coef %>% print(n = nrow(.)) 
+
+sbf_save_table(glance, caption = "Model convergence")
+sbf_save_table(coef, caption = "Model coefficients")
+
+# Predict tail-down semivariogram
+td <- predict(analysis, "H", term = "eTD") %>% 
+  mutate(type = "Tail-down")
+
+gp <- ggplot(td) +
+  geom_line(aes(x = H, y = estimate)) +
+  geom_line(aes(x = H, y = lower), linetype = "dotted") +
+  geom_line(aes(x = H, y = upper), linetype = "dotted") +
+  xlab("Distance (km)") +
+  ylab("Covariance") +
+  NULL
+
+sbf_open_window(3, 2)
+sbf_print(gp)
+
+sbf_save_plot(
+  x_name = "covariance-distance",
+  report = TRUE,
+  caption = "Tail-down covariance by hydrologic distance (with 95% CIs)"
+)
+
+# Plot predicted vs observed
+# preds_vs_obs <-
+#   data %>% 
+#   rename(estimate = water_temp) %>% 
+#   mutate(type = "obs") %>% 
+#   bind_rows(
+#     predict(analysis, new_data = data, term = "eTemp") %>% 
+#       mutate(type = "pred")
+#   )
+
+water_temp <- predict(analysis, new_data = data, term = "eTemp")
+
+gp <- ggplot(water_temp, aes(x = date)) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), fill = pois_cols("grey")) +
+  geom_line(aes(y = estimate), linewidth = 0.2) +
+  geom_point(data = data, aes(y = water_temp), colour = pois_cols("red"), alpha = 0.3, size = 0.5) +
+  facet_wrap(~site, nrow = 8) + 
+  xlab("Date") +
+  ylab(expression(paste("Water Temperature (", degree, "C)"))) +
+  NULL
+
+sbf_open_window(6, 9)
+sbf_print(gp)
+
+sbf_save_plot(
+  x_name = "water-temp",
+  report = TRUE,
+  caption = "Predicted water temperature by date (with 95% CIs). The points are the observed data."
+)
+
+ice_reserve <- predict(analysis, new_data = data, term = "eIceReserve")
+
+gp <- ggplot(ice_reserve, aes(x = date)) +
+  geom_line(aes(y = estimate)) +
+  geom_line(aes(y = lower), linetype = "dotted") +
+  geom_line(aes(y = upper), linetype = "dotted") +
+  facet_wrap(~site, nrow = 6) +
+  xlab("Date") +
+  ylab("Ice Reserve (Joules/m^2)") +
+  NULL
+
+sbf_open_window()
+sbf_print(gp)
+
